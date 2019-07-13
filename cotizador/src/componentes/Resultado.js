39287abeb6fe5b205ad726dc63a8dee3648e41ab
AntonioFl <buceocv@gmail.com>
2019-07-13 20:41:22 +0200

    Finalizado el proyecto básico

--- cotizador/src/componentes/Resultado.js
+++ cotizador/src/componentes/Resultado.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import {TransitionGroup , CSSTransition} from 'react-transition-group';
+
 
 class Resultado extends Component {
 
     render() { 
+        const mensaje = (!this.props.resultado) ? 'Selecciona los valores para cotizar': 'TOTAL '
+
+
         return ( 
-            <p>Resultado :  {this.props.resultado} $</p>
+            <div className="gran-total">
+                {mensaje}
+                <TransitionGroup  component="span" className="resultado">
+                    <CSSTransition
+                        classNames="resultado" 
+                        key= {this.props.resultado}
+                        timeout={{enter: 500 , exit: 500}}>
+                            <span>{this.props.resultado} </span>
+                    </CSSTransition>
+                </TransitionGroup>
+
+            </div>
+
+        
          );
     }
 }
