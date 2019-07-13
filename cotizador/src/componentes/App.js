d29130f0da052870a41400eee366e7d444fabe27
AntonioFl <buceocv@gmail.com>
2019-07-13 20:15:02 +0200

    Creado el Componente  Resumen

--- cotizador/src/componentes/App.js
+++ cotizador/src/componentes/App.js
@@ -2,9 +2,14 @@ import React , {Component} from 'react';
 import Header from './Header';
 import Formulario from './Formulario.js';
 import {obtenerDiferenciaAnio , calcularMarca, obtenerPlan} from '../helper';
+import Resumen from './Resumen';
 
 
 class App extends Component{
+  state = {
+    resultado : '',
+    datos : {}
+  }
 
   cotizarSeguro = (datosCotizacion) =>{
     const {marca , plan , year} = datosCotizacion;
@@ -16,16 +21,27 @@ class App extends Component{
     
     resultado -= ((difAnios * 3 ) * resultado) / 100; 
     
-
     // el vehiculo americano se incrementa 15% el asiatico 5% y el europeo un 30% 
     resultado = calcularMarca(marca) * resultado;
 
     // plan del coche basico incrementa 20% y el completo 50% 
     let incrementoPlan = obtenerPlan(plan);
     
+    // RESULTADO OBTENIDO 
     resultado = parseFloat(incrementoPlan * resultado).toFixed(2);
 
-    console.log(resultado);
+      //Creacion de objeto con los datos para pasarlos al state desde el destructuring (14)
+     const datos = {
+            marca : marca,
+            year : year,
+            plan : plan
+          }
+
+       // carga de resultado en el State 
+      this.setState({
+        resultado : resultado,
+        datos : datos
+      })
     }
 
   render(){
@@ -37,7 +53,12 @@ class App extends Component{
           <Formulario 
             cotizarSeguro = {this.cotizarSeguro}
           />
+          <Resumen
+            datos = {this.state.datos}
+            resultado = {this.state.resultado}
+          />
         </div>
+        
         
       </div>
 
