0eb5d0dc8e87edc4fef578255695eda8c4fdffc5
AntonioFl <buceocv@gmail.com>
2019-07-13 21:14:17 +0200

    Organizado el espacio del DOM para los componentes en App

--- control_gastos_semanal/src/componentes/App.js
+++ control_gastos_semanal/src/componentes/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import '../css/App.css';
+import Header from './Header'
 
 function App() {
   return (
-    <div>HOLA</div>
+      <div className="App container">
+        <Header
+            titulo='GASTO SEMANAL'
+        />
+
+          <div className="contenido-principal contenido">
+            <div className="row">
+
+              <div className="one-half column">
+                
+              </div>
+
+              <div className="one-half column">
+                
+              </div>
+
+            </div>
+          </div>
+
+      </div>
+    
   );
 }
 
