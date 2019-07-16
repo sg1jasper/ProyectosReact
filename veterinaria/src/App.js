d6cea1d06dea077df23a804616f07f1b90a9aefe
AntonioFl <buceocv@gmail.com>
2019-07-16 11:04:28 +0200

    Enviados los datos del Forulario al State de App

--- veterinaria/src/App.js
+++ veterinaria/src/App.js
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react'
+
 import Header from './componentes/Header'
 import AgregarCita from './componentes/AgregarCita'
 
 
-function App() {
-  return (
-    <div className="container">
-      <Header
-        titulo = {'Administrador de pacientes,  Veterinario'}
-      />
-      <div className="row">
-        <div className="col-md-6">
-          <AgregarCita/>
-        </div>
-      </div>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  
+      citas: [] 
+    }
+  }
 
+
+  crearCita = (nuevaCita)=>{
+    const  citas = [...this.state.citas , nuevaCita];
+
+    this.setState({
+      citas : citas 
+    });
+  }
+
+  render() { 
+    return ( 
+        <div className="container">
+          <Header
+            titulo = {'Administrador de pacientes,  Veterinario'}
+          />
+          <div className="row">
+            <div className="col-md-6">
+              <AgregarCita
+                crearCita = {this.crearCita}
+              />
+            </div>
+          </div>
+        </div>
+     );
+  }
+}
+ 
 export default App;
+
--- veterinaria/src/componentes/AgregarCita.js
+++ veterinaria/src/componentes/AgregarCita.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
+import uuid from 'uuid'; // Libreria para generar id unicos por elemento 
 
 class AgregarCita extends Component {
+    
+     // LECTURA DE VALORES DEL FORMULARIO POR MEDIO DE REFS 
+     nombreMascotaRef = React.createRef();
+     nombreDuenoRef = React.createRef();
+     fechaRef = React.createRef();
+     horaRef = React.createRef();
+     sintomasRef = React.createRef();
+
+
     state = {  }
+
+    crearNuevaCita = (e) =>{
+        e.preventDefault();
+        this.props.crearCita();
+       
+
+        const nuevaCita = {
+            id: uuid(),
+            nombreMascota: this.nombreMascotaRef.current.value,
+            nombreDueno: this.nombreDuenoRef.current.value,
+            fecha: this.fechaRef.current.value,
+            hora: this.horaRef.current.value,
+            sintomas: this.sintomasRef.current.value
+        };
+
+        // Envio de objeto nueva cita al padre 
+        this.props.crearCita(nuevaCita);
+
+        //Despues de enviar reseteo el Form 
+        e.currentTarget.reset();
+        
+    }
+
+
+
+
     render() { 
         return ( 
             <div className="card mt-5">
@@ -9,36 +45,36 @@ class AgregarCita extends Component {
                    
                     <h2 className="card-title text-center mb-5"> AGREGA LAS CITAS AQUI </h2>
                   
-                    <form >
+                    <form onSubmit={this.crearNuevaCita}>
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
                             <div className="col-sm-8 col-lg-10">
-                                <input type="text" className="form-control" placeholder="Nombre Mascota" />
+                                <input ref={this.nombreMascotaRef} type="text" className="form-control" placeholder="Nombre Mascota" />
                             </div>
                         </div>
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-2 col-form-label">Nombre Dueño</label>
                             <div className="col-sm-8 col-lg-10">
-                                <input type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" />
+                                <input ref={this.nombreDuenoRef} type="text" className="form-control"  placeholder="Nombre Dueño de la Mascota" />
                             </div>
                         </div>
 
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
                             <div className="col-sm-8 col-lg-4  mb-4 mb-lg-0">
-                                <input type="date" className="form-control" />
+                                <input ref={this.fechaRef} type="date" className="form-control" />
                             </div>                            
 
                             <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
                             <div className="col-sm-8 col-lg-4">
-                                <input type="time" className="form-control" />
+                                <input ref={this.horaRef} type="time" className="form-control" />
                             </div>
                         </div>
 
                         <div className="form-group row">
                             <label className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
                             <div className="col-sm-8 col-lg-10">
-                                <textarea  className="form-control"></textarea>
+                                <textarea ref={this.sintomasRef} className="form-control"></textarea>
                             </div>
                         </div>
                         <div className="form-group row justify-content-end">
