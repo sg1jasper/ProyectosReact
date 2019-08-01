19ea6dfcf420aa876f20f247f2a89a86dca77e0e
AntonioFl <buceocv@gmail.com>
2019-08-01 20:48:56 +0200

    Consulta a api correctamente , guardado n State el objeto retornado por la API

--- reactpixabay/src/App.js
+++ reactpixabay/src/App.js
@@ -3,14 +3,34 @@ import Buscador from './componentes/Buscador';
 
 class App extends Component {
   state = { 
-    termino : ""
+    termino : "" ,
+	resultado : []
+   }
+
+   consultaApi = () =>{
+	   const termino = this.state.termino;
+     const URI = `https://pixabay.com/api/?key=13199340-5394102a1b7ccb87efbb9b9a7&q=${termino}&image_type=photo`;
+
+	 fetch(URI)
+	 	.then((res)=>{
+			 return res.json();
+		 })
+		 .then((data)=>{
+			 this.setState({
+				 resultado : data.hits
+			 })
+		 })
    }
 
   datosBusqueda = (termino) =>{
-    this.setState({
-      termino : termino
-    })
-  };
+		this.setState({
+			termino : termino
+		},()=>{  // Callback que se ejecuta al terminar de insertar en el State 
+				this.consultaApi();
+			}
+		)
+	}
+  
 
   render() { 
 
